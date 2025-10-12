fccd7fde8c7740d8d2bc4bd0f02efd47fc0bf85d
Felipe Souza <felipedb91@gmail.com>
2025-10-12 15:23:25 -0300

    chore: create project

---
+++ src/main/java/br/app/fsantana/marketspaceapi/MarketspaceApiApplication.java
@@ -0,0 +1,13 @@
+package br.app.fsantana.marketspaceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketspaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketspaceApiApplication.class, args);
+	}
+
+}
