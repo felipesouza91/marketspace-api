fb8183fabc873d626e92f6ced3b11eeaefe8091f
Felipe Souza <felipedb91@gmail.com>
2025-10-14 22:24:28 -0300

    feat: add open api docs

---
+++ src/main/java/br/app/fsantana/marketspaceapi/configs/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package br.app.fsantana.marketspaceapi.configs;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "security_auth",
+        type = SecuritySchemeType.HTTP ,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("MarketsPlace API")
+                        .description("API for clients")
+                        .version("0.0.1")
+                        .contact(new Contact()
+                                        .name("Felipe Santana")
+                                        .url( "https://fsantana.dev" )));
+    }
+}
