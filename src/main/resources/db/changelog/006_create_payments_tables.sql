4827623dea3aa709aead3b3d28f30a9b8265c9a8
Felipe Souza <felipedb91@gmail.com>
2025-10-14 22:20:43 -0300

    chore: add security dependencies

--- src/main/resources/db/changelog/006_create_payments_tables.sql
+++ src/main/resources/db/changelog/006_create_payments_tables.sql
@@ -1,14 +1,29 @@
 
 -- CreateTable
 CREATE TABLE "payments_methods" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     "key" TEXT NOT NULL,
     "name" TEXT NOT NULL
 );
 
+INSERT INTO payments_methods(KEY,NAME)
+    VALUES('boleto', 'Boleto');
+
+INSERT INTO payments_methods(KEY,NAME)
+    VALUES('pix', 'Pix');
+
+INSERT INTO payments_methods(KEY,NAME)
+    VALUES('cash', 'Dinheiro');
+
+INSERT INTO payments_methods(KEY,NAME)
+    VALUES('card', 'Cartão de Crédito');
+
+INSERT INTO payments_methods(KEY,NAME)
+    VALUES('deposit', 'Depósito Bancário');
+
 -- CreateTable
 CREATE TABLE "payments_methods_to_products" (
-    "payment_methods_id" TEXT NOT NULL ,
+    "payment_methods_id" UUID NOT NULL ,
     "product_id" UUID NOT NULL ,
     CONSTRAINT "payments_methods_to_products_A_fkey" FOREIGN KEY ("payment_methods_id")
         REFERENCES "payments_methods" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
