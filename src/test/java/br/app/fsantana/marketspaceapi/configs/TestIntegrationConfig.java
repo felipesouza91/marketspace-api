588f78b03ac3d77d47ff4a3f67adf414fa5b57ec
Felipe Souza <felipedb91@gmail.com>
2025-10-27 21:10:36 -0300

    tests: add minio and files base methods

--- src/test/java/br/app/fsantana/marketspaceapi/configs/TestIntegrationConfig.java
+++ src/test/java/br/app/fsantana/marketspaceapi/configs/TestIntegrationConfig.java
@@ -1,30 +1,46 @@
 package br.app.fsantana.marketspaceapi.configs;
 
+import br.app.fsantana.marketspaceapi.domain.dataprovider.FileRepository;
 import br.app.fsantana.marketspaceapi.domain.dataprovider.PaymentMethodRepository;
 import br.app.fsantana.marketspaceapi.domain.dataprovider.ProductDataProvider;
+import br.app.fsantana.marketspaceapi.domain.dataprovider.StorageDataProvider;
 import br.app.fsantana.marketspaceapi.domain.dataprovider.UserDataProvider;
+import br.app.fsantana.marketspaceapi.domain.dataprovider.specifications.ProductSpecs;
 import br.app.fsantana.marketspaceapi.domain.exceptions.AppEntityNotFound;
+import br.app.fsantana.marketspaceapi.domain.exceptions.AppException;
+import br.app.fsantana.marketspaceapi.domain.models.File;
 import br.app.fsantana.marketspaceapi.domain.models.PaymentMethod;
 import br.app.fsantana.marketspaceapi.domain.models.Product;
 import br.app.fsantana.marketspaceapi.domain.models.User;
 import br.app.fsantana.marketspaceapi.secutiry.models.Auth;
 import br.app.fsantana.marketspaceapi.secutiry.services.SessionService;
 import io.restassured.RestAssured;
+import jakarta.persistence.EntityManager;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.MinIOContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 
 import static org.instancio.Select.field;
 
@@ -55,10 +71,32 @@ public abstract class TestIntegrationConfig {
     @Autowired
     private PaymentMethodRepository paymentMethodRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private FileRepository fileRepository;
+
+    @Autowired
+    private StorageDataProvider storageDataProvider;
+
     @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15").withReuse(true);
 
+    @Container
+    static MinIOContainer minioContainer = new MinIOContainer("minio/minio:RELEASE.2025-02-03T21-03-04Z-cpuv1").withReuse(true);
+
+    @DynamicPropertySource
+    public static void configProps(DynamicPropertyRegistry registry) {
+        registry.add("api.storage.minio.bucketName", () -> "test-bucket");
+        registry.add("api.storage.minio.url", minioContainer::getS3URL);
+        registry.add("api.storage.minio.accessKey", minioContainer::getUserName);
+        registry.add("api.storage.minio.accessSecret", minioContainer::getPassword);
+    }
 
     protected User createUser() {
         String blankPassword=  "12345678";
@@ -72,6 +110,23 @@ public abstract class TestIntegrationConfig {
             sessionService.createUser(user);
         }
         user.setPassword(blankPassword);
+        return user;
+    }
+
+    protected User createUserB() {
+        String blankPassword = "12345678";
+        User user = new User();
+        user.setName("Jonh Doe 2 ");
+        user.setEmail("jonhdoe2@email.com");
+        user.setPassword(blankPassword);
+        user.setTel("559999992");
+        user.setPassword(passwordEncoder.encode(blankPassword));
+        Optional<User> savedUser = userDataProvider.
+                findByEmail(user.getEmail());
+        if(savedUser.isPresent()) {
+            return savedUser.get();
+        }
+        userDataProvider.saveAndFlush(user);
         return user;
     }
 
@@ -89,12 +144,46 @@ public abstract class TestIntegrationConfig {
                 .setBlank(field(Product::getUser))
                 .setBlank(field(Product::getPaymentMethods))
                 .setBlank(field(Product::getUser))
+                .set(field(Product::getIsActive), true)
                 .setBlank(field(Product::getPaymentMethods))
                 .setBlank(field(Product::getProductImages))
                 .create();
         product.setUser(user);
         product.getPaymentMethods().add(pix);
         return productDataProvider.saveAndFlush(product);
+    }
+
+    protected List<String> getPaymentKeys() {
+        return List.of("boleto", "pix", "cash", "card", "deposit");
+    }
+
+    protected long countProducts(UUID userId) {
+        return productDataProvider.count(ProductSpecs.withFilter(userId, null));
+    }
+
+    protected File createProductImage(Product product) {
+        try {
+            String updatePath = "products"+ "/" + product.getId().toString();
+            Path path = Path.of("src/test/resources/images/prod1.jpg");
+            java.io.File file = path.toFile();
+            String content =  Files.probeContentType(path);
+
+            File newFile = File.builder()
+                    .path(updatePath)
+                    .fileName(UUID.randomUUID().toString()+ "." + content )
+                    .originalFileName(file.getName())
+                    .contentType(content)
+                    .build();
+
+            fileRepository.save(newFile);
+            String url = storageDataProvider.uploadFile(updatePath, newFile.getFileName(), new FileInputStream(file), content);
+            newFile.setImageUrl(url);
+            product.setProductImages(Set.of(newFile));
+            productDataProvider.save(product);
+            return newFile;
+        } catch (Exception e) {
+            throw new AppException("Error when update files", e);
+        }
     }
 
 }
