c6f69225d0c0c0ef0ef0a3392ed979f4548d69e0
Felipe Souza <felipedb91@gmail.com>
2025-11-26 21:21:44 -0300

    refactor: change method return

--- src/main/java/br/app/fsantana/marketspaceapi/domain/dataprovider/StorageDataProvider.java
+++ src/main/java/br/app/fsantana/marketspaceapi/domain/dataprovider/StorageDataProvider.java
@@ -1,6 +1,7 @@
 package br.app.fsantana.marketspaceapi.domain.dataprovider;
 
 import java.io.InputStream;
+import java.util.Optional;
 
 /**
  * Created by felip on 17/10/2025.
@@ -12,7 +13,7 @@ public interface StorageDataProvider {
 
     boolean fileExits(String path, String fileName);
 
-    String getFileUrl(String path, String fileName);
+    Optional<String> getFileUrl(String path, String fileName);
 
     void deleteFile(String path, String fileName);
 }
